6292c980765f0376efa429309ff2d75e382461c2
jet2018 <ezrajet9@gmail.com>
2021-03-31 20:55:50 +0300

    made some other changes

--- yieldUp/settings.py
+++ yieldUp/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
-    'diagnosis',
     'doctor',
     'profiler',
     'django.contrib.humanize',
